9fef9f111f63af06b5ba5304d17d952042f44b97
RomaRina22 <s22romarina@venta.lv>
2023-02-16 21:19:18 +0200

    izveidoja student

---
+++ src/model/Student.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Student {
+	static int count = 0;
+	private int id;
+	private String name;
+	private String surname;
+	private int[] grades;
+	private char gender;
+	
+	public Student(String name, String surname, int[] grades) {
+		
+	}
+	
+	public Student() {
+		
+	}
+	
+	
+}
