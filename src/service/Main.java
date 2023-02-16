f5fc0b967ff8646eddc30e1161763c0872bb6243
RomaRina22 <s22romarina@venta.lv>
2023-02-16 22:08:28 +0200

    4 studenti sarakstā, kārtošanas tests

--- src/service/Main.java
+++ src/service/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import model.Saraksts;
+import model.Student;
 
 public class Main {
 
@@ -49,6 +50,14 @@ public class Main {
 		sar.sort();
 		System.out.println("Sakārto: "+ sar);
 		
+		Saraksts<Student> studenti = new Saraksts<>(3);
+		studenti.addLast(new Student("Arija", "Gultne",  new int[]{5,4,2}));
+		studenti.addLast(new Student("Boriss", "Straume", new int[] {6,2,3}));
+		studenti.addLast(new Student("Volvo", "Brenss", new int[] {8,7,9}));
+		studenti.addLast(new Student("Jana", "Hello", new int[] {2,2,2}));
+		studenti.print();
+		studenti.sort();
+		studenti.print();
 		
 	}
 	
